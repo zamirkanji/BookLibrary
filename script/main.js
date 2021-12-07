46391f77b31035923f558fee83432be4fc90494e
Zamir Kanji <zamirkanji@comcast.net>
2021-12-06 23:46:44 -0800

    initial commit2

---
+++ script/main.js
@@ -0,0 +1,12 @@
+let arrayOfBooks = [];
+
+function Book (title, author, pages, ifRead) {
+    this.title = title,
+    this.author = author, 
+    this.pages = pages, 
+    this.ifRead = ifRead
+}
+
+function addBookToList () {
+
+}
