131752095b8f9dd996b0c33fd460ca6228cd84e4
Zamir Kanji <zamirkanji@comcast.net>
2021-12-12 18:22:04 -0800

    added dATA key to object Book and card DOM element

--- script/main2.js
+++ script/main2.js
@@ -6,6 +6,7 @@ const formContainer = document.querySelector(".form-container");
 const submitBtn = document.querySelector("#submit");
 const closeBtn = document.querySelector(".close-btn");
 const bookDisplay = document.querySelector(".display-book-cards");
+const myForm = document.querySelector(".form");
 
 //******************************************************** */
 const showForm = () => {
@@ -31,22 +32,24 @@ const removeBlur = () => {
 	headerContainer.classList.remove("blur");
 };
 
-//******************************************************** */
-class Book {
-	constructor(title, author, pages, read, notes) {
-		(this.title = title),
-			(this.author = author),
-			(this.pages = pages),
-			(this.read = read),
-			(this.notes = notes);
-	}
+const randomKey = (max) => {
+	return Math.floor(Math.random() * max);
 }
 
-Book.prototype.setRead = function (ifRead) {
-	if (ifRead === "read") {
-	}
-};
+// let rkey = randomKey(100);
 
+//******************************************************** */
+class Book {
+	constructor(title, author, pages, read, notes, rkey) {
+		(this.title = title),
+		(this.author = author),
+		(this.pages = pages),
+		(this.read = read),
+		(this.notes = notes), 
+		(this.rkey = rkey);
+	}
+}
+/*********************** */
 class Books {
 	constructor() {
 		this.books = [];
@@ -56,29 +59,25 @@ class Books {
 		this.books.push(book);
 	}
 
-	log() {
-		console.log(this.books);
-	}
-
-	deleteBook(title) {
-		this.books = this.books.filter((book) => {
-			log(this.books, "array after filter", title);
-			if (book.title === title) {
-				console.log(true);
-				return true;
-			} else {
-				console.log(false);
-				return false;
-			}
-		});
-		// log(this.books);
-	}
+	// deleteBook(title) {
+	// 	this.books = this.books.filter((book) => {
+	// 		// log(this.books, "array after filter", title);
+	// 		if (book.title === title) {
+	// 			console.log(true);
+	// 			return true;
+	// 		} else {
+	// 			console.log(false);
+	// 			return false;
+	// 		}
+	// 	});
+	// }
 
 	getArray() {
 		return this.books;
 	}
 
-	createCard() {
+	createCard(rkey) {
+		log(this.books, rkey);
 		this.books.forEach((book) => {
 			const bookDiv = document.createElement("div");
 			const infoContainer = document.createElement("div");
@@ -123,84 +122,116 @@ class Books {
 			bookDiv.appendChild(deleteContainer);
 			deleteContainer.appendChild(deleteBookBtn);
 			deleteContainer.appendChild(readToggle);
+
+			const bookCard = document.querySelector('.card');
+			bookCard.setAttribute('data-id', rkey);
+
 		});
 	}
 
-	addKeyToCard() {
-		let temp = null;
-		for (let i = 0; i < this.books.length; i++) {
-			temp = i;
-		}
-		const bookCard = document.querySelector(".card");
-		bookCard.setAttribute("data-id", temp);
-	}
-	addKeyToBook() {
-		for (let i = 0; i < this.books.length; i++) {
-			this.books.data_key = i;
-		}
-		log(this.books.data_key);
-	}
+	// addKeyToCard() {
+	// 	log(books.rkey);
+	// 	const bookCard = document.querySelector(".card");
+	// 	bookCard.setAttribute("data-id", books.rkey);
+	// }
 
-	setRead(read) {
-		if (read === "read") {
-			this.read = "Not Read";
-			readToggle.textContent = "Not Read";
-		} else {
-			this.read = "Read";
-		}
-	}
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //***************************EVENT LISTENERS***************************** */
+const books = new Books();
+
 newBookBtn.addEventListener("click", (e) => {
 	log("test");
 	addBlur();
 	showForm();
 	hideNewBookBtn();
 });
-submitBtn.addEventListener("click", (e) => {
-	e.preventDefault();
-	hideForm();
-	removeBlur();
-	showNewBookBtn();
-	//submit form, logic
-	createBook();
-	
-});
+
 closeBtn.addEventListener("click", (e) => {
 	hideForm();
 	removeBlur();
 	showNewBookBtn();
 });
 
-const createBook = () => {
-	const myForm = document.querySelector(".form");
+submitBtn.addEventListener("click", (e) => {
+	e.preventDefault();
+	hideForm();
+	removeBlur();
+	showNewBookBtn();
+	
+	let rkey = randomKey(100);
+	createBook(rkey);
+});
+
+
+const createBook = (rkey) => {
 	const title = document.querySelector("#title").value;
 	const author = document.querySelector("#author").value;
 	const pages = document.querySelector("#pages").value;
 	const read = document.querySelector('input[name="read"]:checked').value;
 	const notes = document.querySelector("#notes").value;
 
-	const books = new Books();
-	books.addBook(new Book(title, author, +pages, read, notes));
-	books.createCard();
-	books.addKeyToCard();
+	
+	
+	books.addBook(new Book(title, author, +pages, read, notes, rkey));
+	books.createCard(rkey);
+	// books.addKeyToCard();
 
-	const deleteBtns = document.querySelectorAll(".delete-book");
-			deleteBtns.forEach((btn) => {
-			btn.addEventListener("click", () => {
-			books.deleteBook(title);
-			log("test delete btn");
-			});
-		});
 
-	const readBtns = document.querySelectorAll(".read-toggle");
-			readBtns.forEach((btn) => {
-			btn.addEventListener("click", () => {
-			books.setRead(read);
-			log("test read btn");
-		});
-	});
+	// if (rkey === books.rkey) {
+	// 	const bookCard = document.querySelector('.card');
+	// 	bookCard.setAttribute('data-id', rkey);
+	// }
+	// const bookCard = document.querySelector(".card");
+	// bookCard.setAttribute("data-id", rkey);
+
+	// const deleteBtns = document.querySelectorAll(".delete-book");
+	// 		deleteBtns.forEach((btn) => {
+	// 		btn.addEventListener("click", () => {
+	// 		books.deleteBook(title);
+	// 		log("test delete btn");
+	// 		});
+	// 	});
+
+	// const readBtns = document.querySelectorAll(".read-toggle");
+	// 		readBtns.forEach((btn) => {
+	// 		btn.addEventListener("click", () => {
+	// 		// log(books.read);
+	// 		const readToggle = document.querySelector('.read-toggle');
+	// 	if (read === "read") {
+	// 		Book.read = "unread";
+	// 		readToggle.textContent = "Not Read";
+	// 		log(books);
+	// 	} else if (read === "unread") {
+	// 		Book.read = "read";
+	// 		readToggle.textContent = "Read";
+	// 		log(books);
+	// 	} else {
+	// 		return;
+	// 	}
+	// 		log("test read btn");
+	// 	});
+	// });
+
+	
 	myForm.reset();
+
+	return rkey;
 }
 
